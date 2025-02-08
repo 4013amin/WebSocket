bc93c3bc1586c594fb70343b034aa31466ae57aa
delta <amin138400138400@gmail.com>
2025-02-08 16:49:48 +0330

    edi in asgi.py

--- WebSocket/WebSocket/asgi.py
+++ WebSocket/WebSocket/asgi.py
@@ -4,15 +4,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 
-# تنظیم متغیرهای محیطی برای Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebSocket.settings')
 
-# اطمینان از تنظیم شدن Django
 django.setup()
 
 import app.routing
 
-# تعریف application برای ASGI
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # برای درخواست‌های HTTP
     "websocket": AuthMiddlewareStack(  # برای WebSocket
